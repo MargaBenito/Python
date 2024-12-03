2d662ea6913758a8b24f5af142e6719613901757
Marga Benito <CorreData@outlook.es>
2024-12-03 14:06:08 +0000

    Actuaizacion-3-dic-24

---
+++ fundamentos.py
@@ -0,0 +1,16 @@
+''' EJERCICIOS 1
+- Define una variable con valores que se te ocurran e identifica sus tipos. Por ejemplo, números, textos, listas, tuplas, conjuntos, etc. 
+- Crea una tupla que contenga algunos nombres de mascota
+- Crea un diccionario "mascotas", que contenga el nombre y tipo de animal de cada mascota
+- Crea una lista de mascotas en la que cada item contenga nombre, tipo, edad y peso de la mascota
+- Crea un conjunto que represente las distintas secciones de una veterinaria y las mascotas alojadas en cada sección, con sus respectivos datos.
+
+# EJERCICIOS 2
+- Escribe un programa que almacene internamente una lista de números, pida al usuario un número e indique si está en la lista con un True o False.
+- Calcular el sueldo mensual de un operario conociendo la cantidad de horas trabajadas y el valor por hora. Estos valores se deberán solicitar al usuario.
+- Dados N números, separados por "," ingresados por el usuario en una sola entrada, identifica cuál es el mayor de todos.
+- Dada una lista, escribe un programa que cuente los valores únicos.
+- Dada una colección cualquiera, muestra el valor de la clave en segunda posición.'''
+
+v1 = "Que rabia me da cuando estoy perdida"
+v1
